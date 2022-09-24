6939819c8b7afc783b2d76408c09983e3d60410f
SO000000 <hirawatasou@gmail.com>
2022-09-24 18:16:23 +0900

    :sparkles:アンケート結果から名詞と形容詞のみ抽出

--- genImg/genTxt.py
+++ genImg/genTxt.py
@@ -3,7 +3,6 @@ import requests
 import csv
 import request
 
-
 class Words:
     def __init__(self, sentence):
         tagger = MeCab.Tagger("-Owakati")
@@ -11,13 +10,19 @@ class Words:
 
 class Morpheme:
     def __init__(self, words):
-        self.bases = []
+        self.morphemes = []
         tagger = MeCab.Tagger()
         for word in words:
-            results = tagger.parse(word).split()
-            result = results[1]
-            result = result.split(',')
-            self.bases.append((((result[6].split(':'))[-1]).split('/'))[0])
+            results = tagger.parse(word).split(',')
+            self.morphemes.append(results)
+
+def extract(morphemes):
+    result = []
+    for morpheme in morphemes:
+        word, word_class = morpheme[0].split('\t')
+        if word_class == '名詞' or word_class == '形容詞':
+            result.append(word)
+    return result
 
 def translate(txt):
     key = request.key
@@ -27,24 +32,17 @@ def translate(txt):
         "source_lang": 'JA',
         "target_lang": 'EN'
     }
-
     request = requests.post("https://api.deepl.com/v2/translate", data=params)
     result = request.json()
     return result["translations"][0]["text"]
 
-with open('R-PresCo.csv') as f:
+with open('/Users/so/Desktop/R-PresCo2022/genImg/R-PresCo.csv') as f:
     reader = csv.reader(f)
+    words = []
     rows = [row for row in reader]
-    for i in range(len(rows)):
-        if rows[i-1][2] != "ある":
-            del rows[i-1]
-        words = Words(str(rows[i-1][3]))
-        print(words.words)
-        rows[i-1][3] = (Morpheme(words.words).bases)
-        print(rows)
-
-sentence = "入学式で集合写真を撮る"
-words = Words(sentence)
-print(words.words)
-
-morphemes = Morpheme(words.words)
+    del rows[0]
+    for row in rows:
+        if row[2] == 'ある':
+            words = Words(row[3])
+            morphemes = Morpheme(words.words)
+            print(extract(morphemes.morphemes))
